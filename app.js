15c5ba6460d90c783e5ef9c0d131b01a28108005
Jonah <Jonah147>
2018-12-25 21:46:17 +0300

    main commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const TOKEN = '657627110:AAHmqbjUTmGfDmrZW34zNLfTZXaq-uckMHs'
+// const TOKEN = '735717824:AAHBPxaN_sKZZyJcHtlBANgVLg0nRFjI8fA';
 
 var express = require('express');
 var app = express();
@@ -17,7 +18,7 @@ var admin = require('firebase-admin');
 admin.initializeApp({
   credential: admin.credential.applicationDefault()
 });
-channelId = "-1001241155007"
+adminId = "288870772";
 // var tasks = [];
 // function getTasks() {
 //   return db.ref('/tasks/').once('value').then(function(snapshot) {
@@ -68,8 +69,7 @@ bot.onText(/\/start/,(msg) => {
         "reply_markup": {
             "one_time_keyboard": true,
             keyboard: [[{
-                text: "share contact", request_contact: true }],
-                ["cancel"]
+                text: "share contact", request_contact: true }]
             ]
         }
       }
@@ -84,29 +84,29 @@ bot.onText(/\/start/,(msg) => {
             }
           }
           bot.sendMessage(msg.chat.id, "How certain are you to come?", option_two);
-          bot.once("callback_query",(msg) => {
-            if(msg.data == 'coming') {
-              attendee.Status = "coming";
-            } else {
-              attendee.Status = "not sure";
-            }
-            bot.sendMessage(msg.message.chat.id, "Thank you for registering, we will update you with the dethails very soon...");
-            // bot.sendMessage(channelId,`Name: ${ attendee.FullName}, 
-            //                                 Phone: ${ attendee.phone_number}
-            //                                 Telegram: @${ attendee.telegramName},`);
-            db.collection('attendees').doc(attendee.FullName + attendee.telegramId).set(attendee);
-          })
+          
+      bot.on("callback_query",(msg) => {
+        if(msg.data == 'coming') {
+          attendee.Status = "coming";
+        } else if(msg.data == "onhold"){
+          attendee.Status = "not sure";
+        } else if(msg.data == "cancel_share") {
+          bot.sendMessage(msg.message.chat.id, "Thank you for registering, we will update you with the dethails very soon...");
+        }
+        bot.sendMessage(adminId,`Name: ${ attendee.FullName}, Phone: ${ attendee.PhoneNumber } Telegram: @${ attendee.telegramName},`);
+          db.collection('attendees').doc(attendee.FullName + attendee.telegramId).set(attendee);
       })
-      
-    });
+    });  
+      })
 });
 
 var http = require('https');
+var path = require('path');
 setInterval(function() {
   var res = http.get('https://gwaguz.herokuapp.com/');
 },30000);
 app.get('/',(req,res) => {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
